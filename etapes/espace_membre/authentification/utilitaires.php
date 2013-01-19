955a8bb261e9da8836bb720c3ad1c9bea5b43d74
Kwentine <quentin.agren@gmail.com>
2013-01-19 18:43:36 +0100

    Fonctions pour effectuer des requêtes paramétrés.
    utilitaires.php : contient deux fonctions qu permettent de sécuriser les données transmises par l'utilisateur, et de les passer en paramètre à une requête SQL.
    A l'aide de ces fonctions, j'ai réécrit la partie logique de 'identification.php'.
    
    creation_tables.sql :
    J'ai redéfini la base clubrock_membres en utilisant des 'enum' lorsqu'il y a un nombre fini de valeurs possibles pour un champ.
    Et ajouté un utilisateur 'clubrock_admin' (mdp : 'chachacha' ) qui a tous les droits sur les tables de la base clubrock.

---
+++ etapes/espace_membre/authentification/utilitaires.php
@@ -0,0 +1,49 @@
+<?php
+// Pour que la fonction s'exécute sans erreurs, il faut que 
+// toutes les clefs figurent dans le tableau
+
+function securise_donnees ( $cles, $tableau )
+{
+  $valeurs_securisees = array ();
+  
+  // Si 'mdp' fait partie des clefs, on chiffre le mot de passe
+  // puis on l'enlève du tableau
+  
+  $position = array_search('mdp',$cles);
+
+  if($position!==false){
+    $valeurs_securisees[':mdp'] = sha1( $tableau['mdp'] ) ;
+    unset($cles[$position]);}    
+   
+  foreach( $cles as $cle)
+    $valeurs_securisees[':'.$cle] = htmlspecialchars($tableau[$cle]);
+  
+  return $valeurs_securisees ;
+
+}
+
+//Pour tester :  
+//$cles = array( 'mdp','uid' );
+//$donnees = array('mdp'=>'toto' , 'uid'=>'<em> titi </em>'); 
+//$donnees_securisees = securise_donnees( $cles , $donnees) ;
+//print_r( $donnees_securisees );
+
+
+function execute_requete($liaison, $sql , $parametres) 
+{
+  $requete = $liaison -> prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY) ) ;
+  $requete -> execute($parametres);
+  
+  return $requete ;
+}
+
+// Pour tester 
+
+$liaison = new PDO('mysql:host=localhost; dbname=clubrock', 'clubrock_admin','chachacha'); 
+$donnees = array('pseudo'=>'tutq', 'mdp'=>'tata');
+$cles = array('pseudo', 'mdp');
+$donnees_securisees = securise_donnees( $cles , $donnees) ;
+$sql="INSERT INTO test values(:pseudo , :mdp)";
+execute_requete($liaison, $sql, $donnees_securisees );
+
+?>
